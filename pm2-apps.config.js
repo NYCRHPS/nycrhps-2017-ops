fee9f73ead24aeb2e7d564df71784c9cea11dce8
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2017-02-13 14:29:35 -0500

    fix config

--- pm2-apps.config.js
+++ pm2-apps.config.js
@@ -1,36 +1,36 @@
 module.exports = {
-  apps : [{
-    name        : "website",
-    script      : "index.js",
-    cwd			: "/home/ubuntu/nycrhps-2017/",
-    watch       : true,
-    env: {
-      "NODE_ENV": "development",
-    },
-    env_production : {
-       "NODE_ENV": "production"
-    }
-  }, {
-    name        : "admin",
-    script      : "index.js",
-    cwd			: "/home/ubuntu/nycrhps-2017-admin/",
-    watch       : true,
-    env: {
-      "NODE_ENV": "development",
-    },
-    env_production : {
-       "NODE_ENV": "production"
-    }
-  }, {
-    name        : "casting",
-    script      : "index.js",
-    cwd			: "/home/ubuntu/nycrhps-casting/",
-    watch       : true,
-    env: {
-      "NODE_ENV": "development",
-    },
-    env_production : {
-       "NODE_ENV": "production"
-    }
-  }]
+	apps: [{
+		name: "website",
+		script: "index.js",
+		cwd: "/home/ubuntu/nycrhps-2017/",
+		watch: true,
+		env: {
+			"NODE_ENV": "development",
+		},
+		env_production: {
+			"NODE_ENV": "production"
+		}
+	}, {
+		name: "admin",
+		script: "index.js",
+		cwd: "/home/ubuntu/nycrhps-2017-admin/",
+		watch: true,
+		env: {
+			"NODE_ENV": "development",
+		},
+		env_production: {
+			"NODE_ENV": "production"
+		}
+	}, {
+		name: "casting",
+		script: "index.js",
+		cwd: "/home/ubuntu/nycrhps-casting/",
+		watch: true,
+		env: {
+			"NODE_ENV": "development",
+		},
+		env_production: {
+			"NODE_ENV": "production"
+		}
+	}]
 };
