f8dda2c275b1a89b586c0056e4b7ea3af7de1152
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2017-06-04 14:33:04 -0400

    update create ssl cert

--- create-ssl-prod.sh
+++ create-ssl-prod.sh
@@ -1,5 +1,5 @@
 mkdir /var/www/www.nycrhps.org
-sudo letsencrypt certonly --webroot -w /var/www/www.nycrhps.org -d www.nycrhps.com -d nycrhps.com -d nycrhps.org -d www.nycrhps.org
+sudo letsencrypt certonly --webroot -w /var/www/www.nycrhps.org -d nycrhps.org -d www.nycrhps.org -d www.nycrhps.com -d nycrhps.com
 
 (sudo crontab -l 2>/dev/null; echo "30 2 * * 1 /usr/bin/letsencrypt renew >> /var/log/le-renew.log")| sudo crontab -
 
