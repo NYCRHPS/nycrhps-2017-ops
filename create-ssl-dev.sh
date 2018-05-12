117b29441b6ce5dac47e0121cfc86df24ece8cd5
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2018-05-12 19:31:26 -0400

    update crontab config after cert creation to restart nginx the day after a cert is renewed

--- create-ssl-dev.sh
+++ create-ssl-dev.sh
@@ -3,4 +3,6 @@ sudo letsencrypt certonly --webroot -w /var/www/dev.nycrhps.com -d dev.nycrhps.c
 
 (sudo crontab -l 2>/dev/null; echo "30 2 * * 1 /usr/bin/letsencrypt renew >> /var/log/le-renew.log")| sudo crontab -
 
+(sudo crontab -l 2>/dev/null; echo "31 2 * * 1 sudo service nginx restart")| sudo crontab -
+
 sudo service nginx restart;
--- create-ssl-prod.sh
+++ create-ssl-prod.sh
@@ -3,4 +3,6 @@ sudo letsencrypt certonly --webroot -w /var/www/www.nycrhps.org -d nycrhps.org -
 
 (sudo crontab -l 2>/dev/null; echo "30 2 * * 1 /usr/bin/letsencrypt renew >> /var/log/le-renew.log")| sudo crontab -
 
+(sudo crontab -l 2>/dev/null; echo "31 2 * * 1 sudo service nginx restart")| sudo crontab -
+
 sudo service nginx restart;
